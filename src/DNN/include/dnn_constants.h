b72ef35a542cea6f05eaba72056b5191e142cff1
= <=>
2015-07-09 16:38:24 +0200

    added header for constants

---
+++ src/DNN/include/dnn_constants.h
@@ -0,0 +1,13 @@
+#ifndef DNN_CONSTANTS_H
+#define DNN_CONSTANTS_H
+
+//dnn settings
+#define LAYER_SIZE 28*28
+#define LAYER_STACK_SIZE 2
+#define OUTPUT_LAYER_SIZE 6
+#define LEARNING_RATE 0.2 //should be 0<=alpha<=1
+
+
+
+
+#endif
